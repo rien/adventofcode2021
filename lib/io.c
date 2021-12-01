706b2fa48df027c6ca0bf730b50f8d568c4d2afc
Rien Maertens <rien.maertens@posteo.be>
2021-12-01 23:59:31 +0100

    prepare io for tomorrow

--- lib/io.c
+++ lib/io.c
@@ -5,43 +5,51 @@
 
 #define IBUF_SIZE 12288
 
+static long int left;
+static long int bidx = IBUF_SIZE;
+static unsigned char buff[IBUF_SIZE];
+
+static inline long int read_char() {
+    bidx += 1;
+    /* fill buffer if required, ensures at least 1 char */
+    if (bidx >= left) {
+        left = read(STDIN_FILENO, &buff, IBUF_SIZE);
+        bidx = 0;
+    }
+    return left;
+}
+
+/**
+ * The current character from stdout.
+ * Undefined if there hasn't been a call to read_char() yet.
+ */
+static inline unsigned char current_char() {
+    return buff[bidx];
+}
+
 /**
  * Read the next integer and store it in *pointer.
+ * After this function has been performed, the current_char() will be the first
+ * non-digit character from stdout.
  *
  * Returns `true` if an integer has been read.
  * Returns `false` if stdin is empty (and no integer has been read).
  */
 static inline int read_uint(unsigned int* pointer) {
-    static long int left;
-    static long int bidx = IBUF_SIZE;
-    static char buff[IBUF_SIZE];
 
-    char nxt;
+    unsigned char nxt;
     unsigned int val = 0;
 
-    while (true) {
-
-        /* fill buffer if required, ensures at least 1 char */
-        if (bidx >= left) {
-            left = read(STDIN_FILENO, &buff, IBUF_SIZE);
-            bidx = 0;
-            /* there are no more bytes to read */
-            if (left == 0) {
-                return false;
-            }
-        }
-
-        /* consume a single character */
-        nxt = buff[bidx];
-        bidx += 1;
-
-        if (nxt != '\n') {
-            val = val*10 + (unsigned int) (nxt - 48);
+    while (read_char()) {
+        nxt = current_char() - 48;
+        if (nxt < 10) {
+            val = val*10 + (unsigned int) nxt;
         } else {
             *pointer = val;
             return true;
         }
     }
+    return false;
 }
 
 /**
